e4c53bacf42561bba4bc1af2971403013f9bba81
LingKong2010 <lingkong20100120@163.com>
2024-10-06 18:21:21 +0800

    Update Odd_and_even_number_judges.java

--- Odd_and_even_number_judges.java
+++ Odd_and_even_number_judges.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;   //导入包
-public class Odd_和_even_number_judges {
+public class Odd_and_even_number_judges {
     public static void main(String[] args) {
         System.out.println("奇数偶数判断器(整数)");
             System.out.println("请输入数字以判断偶数/奇数：");
